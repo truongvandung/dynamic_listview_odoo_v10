c28b6b8cdc81a7528fe327f44ba6bc1e0f071e8a
dungtv <dungtv@elephas.vn>
2017-09-15 18:05:06 +0700

    f

---
+++ su_dynamic_listview_odoo_v10/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'Dynamic ListView Odoo 10 Advance',
+    'summary': 'Dynamic ListView Odoo 10 Advance',
+    'version': '1.0',
+    'category': 'Web',
+    'description': """
+       Dynamic ListView Odoo 10 Advance
+    """,
+    'author': "startup",
+    'depends': ['web'],
+    'data': [
+        'views/templates.xml',
+        'security/show_fields_security.xml',
+        'security/ir.model.access.csv',
+    ],
+    'qweb': [
+        'static/src/xml/base.xml',
+    ],
+    'images': ['images/main_screen.jpg'],
+    'price': 128,
+    'currency': 'EUR',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'images': [
+        'static/description/module_image.png',
+    ],
+}
