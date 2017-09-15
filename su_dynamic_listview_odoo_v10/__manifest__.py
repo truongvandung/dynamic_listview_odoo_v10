103dc523eb9ed0617b7ad4596c131d462efebd5f
dungtv <dungtv@elephas.vn>
2017-09-15 18:07:38 +0700

    f

--- su_dynamic_listview_odoo_v10/__manifest__.py
+++ su_dynamic_listview_odoo_v10/__manifest__.py
@@ -17,7 +17,7 @@
         'static/src/xml/base.xml',
     ],
     'images': ['images/main_screen.jpg'],
-    'price': 128,
+    'price': 180,
     'currency': 'EUR',
     'installable': True,
     'auto_install': False,
