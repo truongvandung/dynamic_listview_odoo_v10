63de0249ba14975d1dea47eb7c624a59f88fcc6c
truongvandung <truongdung.vd@gmail.com>
2017-11-15 13:56:58 +0700

    w
    w

--- su_dynamic_listview_odoo_v10/__manifest__.py
+++ su_dynamic_listview_odoo_v10/__manifest__.py
@@ -1,10 +1,10 @@
 {
-    'name': 'Dynamic ListView Odoo 10 Advance',
-    'summary': 'Dynamic ListView Odoo 10 Advance',
+    'name': 'POS LIKE',
+    'summary': '',
     'version': '1.0',
     'category': 'Web',
     'description': """
-       Dynamic ListView Odoo 10 Advance
+       
     """,
     'author': "startup",
     'depends': ['web'],
